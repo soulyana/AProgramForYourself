f3f917d0d4aa3182b202e479a3da1e3f8517bb13
Soulyana <Soulyana3@gmail.com>
2017-10-02 14:32:41 -0400

    Initial commit

---
+++ src/AProgramForYourself.java
@@ -0,0 +1,9 @@
+public class AProgramForYourself {
+    public static void main(String args[]) {
+        //Soulyana Lakew, age 29
+        String name = "Soulyana Lakew";
+        int age = 29;
+
+        System.out.println(name + " " + age);
+    }
+}
