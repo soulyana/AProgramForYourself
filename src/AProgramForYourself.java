bc589ed205f8e8f9b5a644022c502e4ff8c91dfb
Soulyana <Soulyana3@gmail.com>
2017-10-02 14:53:41 -0400

    second commit

--- src/AProgramForYourself.java
+++ src/AProgramForYourself.java
@@ -1,9 +1,13 @@
 public class AProgramForYourself {
     public static void main(String args[]) {
         //Soulyana Lakew, age 29
-        String name = "Soulyana Lakew";
+        String firstName = "Soulyana";
+        String lastName = "Lakew";
         int age = 29;
+        String movie = "Dave Chappelle";
+        double GPA = 4.0;
 
-        System.out.println(name + " " + age);
+        System.out.println(firstName + " " + lastName + ", " + age + ". The last movie I saw was " + movie
+                + ". My GPA is " + GPA  );
     }
 }
